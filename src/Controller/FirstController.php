b6c376a1cb26558fb1c54c6ad272a702d082662d
imane <imanetata2003@gmail.com>
2023-05-08 20:05:35 +0100

    update1

---
+++ src/Controller/FirstController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class FirstController extends AbstractController
+{
+    #[Route('/first/imane', name: 'app_first')]
+    
+    public function index(): Response
+    {
+        
+        return $this->render('first/index.html.twig',[
+            'firstname'=>'imane',
+            'name'=>'tata'
+        ]);
+    }
+    #[Route('/sayHello/{name}/{firstname}', name: 'say.hello')]
+    
+    public function sayHello( Request $request, $name,$firstname): Response
+    { 
+        dd($request);
+       return $this->render('first/hello.html.twig',[
+       'name'=>$name,
+       'firstname'=>$firstname
+       ]);
+    }
+   
+}
