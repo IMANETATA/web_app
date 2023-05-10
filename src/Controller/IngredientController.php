a19d76fd2bd0a18f4ea2441192c5a2dfc91576e0
imane <imanetata2003@gmail.com>
2023-05-10 17:46:26 +0100

    🎨 updates

--- src/Controller/IngredientController.php
+++ src/Controller/IngredientController.php
@@ -80,12 +80,42 @@ class IngredientController extends AbstractController
      */
 #[Route('ingredient/edition/{id}',name:'ingredient.edit',methods:['GET','POST'])]
 
-    public function edit(Ingredient $ingredient):Response{
+    public function edit(Ingredient $ingredient,Request $request,EntityManagerInterface $manager):Response{
        
         $form= $this->createForm(IngredientType::class,$ingredient);
-       
+        $form->handleRequest($request);
+        
+        if($form->isSubmitted() &&  $form->isValid() ){
+            $ingredient=$form->getData();
+            
+            $manager->persist($ingredient);//ajout a la base
+           $manager->flush();
+           $this->addFlash(
+            'success',
+            'votre ingredient a ete modifié avec success!'
+        );
+        return $this->redirectToRoute('ingredient.index');
+        }
         return $this->render('pages/ingredient/edit.html.twig',[
             'form'=>  $form->createView()
         ]);
+    }
+#[Route('/ingredient/supression/{id}',name:'ingredient.delete',methods:['POST'])]
+    public function delete(EntityManagerInterface $manager,Ingredient $ingredient):Response {
+        if(!$ingredient){
+            $this->addFlash(
+                'warning',
+                'votre ingredient n a pas ete trouvé!'
+            );
+            return $this->redirectToRoute('ingredient.index');
+
+        }
+        $manager->remove($ingredient);
+        $manager->flush();
+        $this->addFlash(
+            'success',
+            'votre ingredient a ete supprimé avec success!'
+        );
+        
     }
 }
